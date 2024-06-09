160d2b77bcf0f2e5b3267735f5e6f2d080034432
Nikita <115003350+kikoser@users.noreply.github.com>
2024-06-09 15:06:00 +0300

    Update api.py

--- spaceship/routers/api.py
+++ spaceship/routers/api.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter
+import numpy as np
 
 router = APIRouter()
 
@@ -6,3 +7,16 @@ router = APIRouter()
 @router.get('')
 def hello_world() -> dict:
     return {'msg': 'Hello, World!'}
+
+
+@router.get("matrix")
+def mult_matrices() -> dict:
+    matrix1 = np.random.rand(10, 10)
+    matrix2 = np.random.rand(10, 10)
+
+    product = np.matmul(matrix1, matrix2)
+    return {
+        "matrix_a": matrix1.tolist(),
+        "matrix_b": matrix2.tolist(),
+        "product": product.tolist(),
+    }
